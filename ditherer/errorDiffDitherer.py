e5c08f20b93904e60e13030ba79f37c7b5f79ca4
snailspot <kousukekuno@hotmail.com>
2026-06-23 22:44:43 +0930

    Added a grayscale method to convert all images to grayscale before dithering.

--- ditherer/errorDiffDitherer.py
+++ ditherer/errorDiffDitherer.py
@@ -6,7 +6,7 @@ import numba
 
 class errorDiffusion():
 
-    def dither(self, filePath, thresholdValue=0.5):
+    def dither(self, filePath, thresholdPercent=50):
         validFile = False
         try:
             image = Image.open(filePath)
@@ -15,14 +15,22 @@ class errorDiffusion():
             pass
 
         if validFile:
-            imageData = np.array(image).astype(np.float32) / 255.
-            if len(imageData.shape) == 2:
-                imageData = imageData[:, :, np.newaxis]
+            imageData = np.array(image).astype(np.uint8)
+            if len(imageData.shape) == 3:
+                imageData = self.__toGrayscale(imageData)
 
-            imageData = self.__threshold(imageData, thresholdValue)
+            imageData = self.__threshold(imageData, thresholdPercent)
 
-            plt.imshow(imageData[:, :, 0], cmap='Greys_r')
+            plt.imshow(imageData[:, :], cmap='gray')
             plt.show()
 
-    def __threshold(self, pixArray, value=0.5):
-        return np.where(pixArray > value, 1, 0)
+    def __threshold(self, pixArray, value):
+        normalisedValue = value * 2.55
+        return np.where(pixArray > normalisedValue, np.iinfo(np.uint8).max, 0)
+
+    def __toGrayscale(self, pixArray):
+        # based on the following grayscale formula gray = 0.3 * R + 0.59 * G + 0.11 * B
+        grayValue = 0.3 * pixArray[:, :, 0] + 0.59 * \
+            pixArray[:, :, 1] + 0.11 * pixArray[:, :, 2]
+        grayImg = grayValue.astype(np.uint8)
+        return grayImg
--- main.py
+++ main.py
@@ -2,5 +2,5 @@ from ditherer.errorDiffDitherer import errorDiffusion
 
 
 ed = errorDiffusion()
-filepath = r"assets\testInputBW.png"
-ed.dither(filepath, 0.39)
+filepath = r"assets\testInputColour.png"
+ed.dither(filepath)
